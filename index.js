d7a71cc8ae5882a4038971d006f9ff17eb7490fd
litochee <litochee@icloud.com>
2017-05-28 23:23:44 -0400

    First Commit Logger
    Make sure to install
    discord js, sqlite and moment .

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+const moment = require('moment');
+const sql = require('sqlite');
+
+sql.open('./cordLogs.sqlite');
+
+client.login(config.token);
+
+client.on('ready', () =>{
+  console.log('Logging');
+})
+
+client.on('message', message =>{
+  //check channel from the message recieved SMART
+//console.log(message.channel.name);// finds channel name
+let tStamp = moment().format('LLLL');
+console.log(tStamp);
+
+sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(() =>{
+  console.error;
+  sql.run(`CREATE TABLE IF NOT EXISTS ${message.channel.name} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() =>{
+    sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
+  })
+})
+
+});
