f85b093481b56746162303f4906a8c0613ecfd9b
litochee <litochee@users.noreply.github.com>
2017-05-29 01:28:50 -0400

    implemented DMs
    now will create new tables for DMs and will record them.

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 This discord bot made in Discord JS will copy any messages and log them into sqlite db.
 
 Format
-each table will represent a text channel. All logs are dated, and usernames are inputted.
+each table will represent a text channel. All logs are dated, and usernames are inputted. Now Handles DMs!
 
 Future updates: return x amount of messages from user.
 
 #Installing
 
-run on node: npm install --save discord.js , npm install --save sqlite , npm install --save moment
->will install discord.js sqlite, and moment
-
-run> "node index.js" to start and it will start logging!
+1. Open config.json and update with your token code
+2. run on node: 
+>npm install --save discord.js
+>npm install --save sqlite
+>npm install --save moment
+3.Open node command prompt and type "node index.js" to start and it will start logging!
--- index.js
+++ index.js
@@ -16,13 +16,20 @@ client.on('message', message =>{
   //check channel from the message recieved SMART
 //console.log(message.channel.name);// finds channel name
 let tStamp = moment().format('LLLL');
-console.log(tStamp);
-
-sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(() =>{
-  console.error;
-  sql.run(`CREATE TABLE IF NOT EXISTS ${message.channel.name} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() =>{
-    sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
+//console.log(tStamp);
+if (message.channel.type == 'dm'){
+  let dmName = `${message.author.username}DM`;
+  sql.run(`INSERT INTO ${dmName} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(()=>{
+    sql.run(`CREATE TABLE IF NOT EXISTS ${dmName} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() =>{
+      sql.run(`INSERT INTO ${dmName} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
+    })
   })
-})
-
+}else{
+  sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(() =>{
+    console.error;
+    sql.run(`CREATE TABLE IF NOT EXISTS ${message.channel.name} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() =>{
+      sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
+    })
+  })
+}
 });
