af18132f0c2211e197e6bc106808f04c6d5a684d
Zachery <zachery@foxboy.sh>
2021-05-05 18:06:55 -0500

    Refactor code

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('./config.json');
-const moment = require('moment');
-const sql = require('sqlite');
+import { Client } from "discord.js";
+const client = new Client();
+import { token } from "./config.json";
+import { open, run } from "sqlite";
+require("dotenv").config();
 
-sql.open('./cordLogs.sqlite');
+// Login to Discord
+client.login(process.env.TOKEN);
 
-client.login(config.token);
+// Setup SQL
+open("./cordLogs.sqlite");
 
-client.on('ready', () =>{
-  console.log('Logging');
-})
+// First, we will use the channel id just incase two channels happen to have the same name, it's bad to use names as ids anyway.
+// Secondly, We're going to store time in the form of a unix timestamp (seconds since 1970-01-01 00:00:00 UTC) as it's the easiest to work with
+// Lastly, we're going to put this all in one table, there's no need to make a separate table for DMs when we can just have a boolean (sqlite doesn't have booleans, so we use 0 and 1)
 
-client.on('message', message =>{
-  //check channel from the message recieved
-let tStamp = moment().format('LLLL'); //creates time stamp
+// Let's setup our table here so we don't have to do checks for it later.
+run(
+	"CREATE TABLE IS NOT EXISTS messages (id INTEGER PRIMARY KEY), channel_id INTEGER NOT NULL, message TEXT NOT NULL, user_id INTEGER NOT NULL, timestamp INTEGER NOT NULL, is_dm INTEGER"
+);
 
-if (message.channel.type == 'dm'){ //checks for DM - Creates a DM DB and records
-  let dmName = `${message.author.username}DM`;
-  sql.run(`INSERT INTO ${dmName} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(()=>{
-    sql.run(`CREATE TABLE IF NOT EXISTS ${dmName} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() =>{
-      sql.run(`INSERT INTO ${dmName} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
-    })
-  })
-}else{ //otherwise it's a normal channel
-  sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(() =>{
-    console.error;
-    sql.run(`CREATE TABLE IF NOT EXISTS ${message.channel.name} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() =>{
-      sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
-    }) // KNOWN ISSUE: Does not like channels with "-" in them
-  })
-}
+client.on("ready", () => {
+	console.log("Watching for messages...");
+});
+
+client.on("message", (message) => {
+	// Let's go ahead and log that we received a message from someone
+	console.log(
+		`Message received in channel ${message.channel.name} (${message.channel.id}) from ${message.author.name}`
+	);
+
+	// Insert the message into our database
+	run(
+		"INSERT INTO messages (channel_id, message, user_id, is_dm) VALUES (?, ?, ?, ?)",
+		[
+			message.channel.id,
+			message.content,
+			message.author.id,
+			message.channel.type == "dm" ? 1 : 0, // We don't want to insert a boolean into the INTEGER row, so let's convert it.
+		]
+	).catch((ex) => {
+		// If there is some how an error with inserting, let the user know!
+		console.log(
+			"There was an issue inserting this message into the database! Please check your connection"
+		);
+
+		// Let's also log the error generated
+		console.log(ex);
+	});
 });
