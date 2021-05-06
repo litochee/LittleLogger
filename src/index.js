f14d18e4edc1c2ff2bb59b62d95178696149a893
Zachery <zachery@foxboy.sh>
2021-05-05 23:51:02 -0500

    More refactoring

--- src/index.js
+++ src/index.js
@@ -1,70 +1,95 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
 const sql = require("sqlite");
 const sqlite3 = require("sqlite3");
 require("dotenv").config();
 
-(async () => {
-	// Login to Discord
-	client.login(process.env.TOKEN);
+// Setup our database
+const db = sql.open({
+	filename: "./cordLogs.db",
+	driver: sqlite3.cached.Database,
+});
 
-	const db = sql
-		.open({
-			filename: "./cordLogs.db",
-			driver: sqlite3.cached.Database,
-		})
-		.then((db) => {
-			// First, we will use the channel id just incase two channels happen to have the same name, it's bad to use names as ids anyway.
-			// Secondly, We're going to store time in the form of a unix timestamp (seconds since 1970-01-01 00:00:00 UTC) as it's the easiest to work with
-			// Lastly, we're going to put this all in one table, there's no need to make a separate table for DMs when we can just have a boolean (sqlite doesn't have booleans, so we use 0 and 1)
+// Make sure our table is setup
+db.then((db) => {
+	db.run(
+		"CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, channel_id INTEGER NOT NULL, message TEXT NOT NULL, user_id INTEGER NOT NULL, timestamp INTEGER NOT NULL, is_dm INTEGER)"
+	);
+});
 
-			// Let's setup our table here so we don't have to do checks for it later.
-			db.run(
-				"CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, channel_id INTEGER NOT NULL, message TEXT NOT NULL, user_id INTEGER NOT NULL, timestamp INTEGER NOT NULL, is_dm INTEGER)"
+/// END DATABASE SETUP ///
+
+// Login to Discord
+const client = new Discord.Client();
+
+client.login(process.env.TOKEN);
+
+client.on("ready", () => {
+	console.log("Watching for messages...");
+});
+
+// Await messages
+client.on("message", (message) => {
+	logMessage(message);
+});
+
+/// END DISCORD SETUP ///
+
+/**
+ * Log a message sent in a channel
+ * @param {*} message to log
+ */
+async function logMessage(message) {
+	// If the message is an attachment, let's ignore it.
+	// TODO: If the attachment comes with a message, let's store that
+	if (message.attachments.size > 0) {
+		console.log(
+			`Attachment from ${message.author.username} (${message.author.id}) received, ignoring...`
+		);
+		return;
+	}
+
+	// Let's go ahead and log that we received a message from someone
+	switch (message.channel.type) {
+		case "dm":
+			console.log(
+				`Message received in DM (${message.channel.id}) from '${message.author.username}' (${message.author.id})`
+			);
+			break;
+		case "text":
+			console.log(
+				`Message received in channel '#${message.channel.name}' (${message.channel.id}) from '${message.author.username}' (${message.author.id})`
+			);
+			break;
+	}
+
+	// Insert the message into our database
+	insertMessage(db, message);
+}
+
+/**
+ * Insert a message into the sqlite database
+ * @param {*} db sqlite database
+ * @param {*} message to insert
+ */
+async function insertMessage(db, message) {
+	db.then((db) => {
+		db.run(
+			"INSERT INTO messages (channel_id, message, user_id, is_dm, timestamp) VALUES (?, ?, ?, ?, ?)",
+			[
+				message.channel.id,
+				message.content,
+				message.author.id,
+				message.channel.type == "dm" ? 1 : 0, // We don't want to insert a boolean into the INTEGER type, so let's convert it.
+				Date.now(),
+			]
+		).catch((ex) => {
+			// If there is some how an error with inserting, let the user know!
+			console.log(
+				"There was an issue inserting this message into the database!"
 			);
 
-			client.on("ready", () => {
-				console.log("Watching for messages...");
-			});
-
-			client.on("message", (message) => {
-				if (message.attachments.size > 0) {
-					console.log(
-						`Attachment from ${message.author.username} (${message.author.id}) received, ignoring...`
-					);
-					return;
-				}
-
-				// Let's go ahead and log that we received a message from someone
-				if (message.channel.type == "dm") {
-					console.log(
-						`Message received in DM (${message.channel.id}) from '${message.author.username}' (${message.author.id})`
-					);
-				} else {
-					console.log(
-						`Message received in channel '#${message.channel.name}' (${message.channel.id}) from '${message.author.username}' (${message.author.id})`
-					);
-				}
-
-				// Insert the message into our database
-				db.run(
-					"INSERT INTO messages (channel_id, message, user_id, is_dm, timestamp) VALUES (?, ?, ?, ?, ?)",
-					[
-						message.channel.id,
-						message.content,
-						message.author.id,
-						message.channel.type == "dm" ? 1 : 0, // We don't want to insert a boolean into the INTEGER type, so let's convert it.
-						Date.now(),
-					]
-				).catch((ex) => {
-					// If there is some how an error with inserting, let the user know!
-					console.log(
-						"There was an issue inserting this message into the database!"
-					);
-
-					// Let's also log the error generated
-					console.log(ex);
-				});
-			});
+			// Let's also log the error generated
+			console.log(ex);
 		});
-})();
+	});
+}
